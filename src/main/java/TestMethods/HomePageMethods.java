bba10a12b216d41ca317261bbc2fb54f1fbd8068
priyadharshini <rpd15996@gmail.com>
2023-07-10 21:28:45 +0200

    final commit

--- src/main/java/TestMethods/HomePageMethods.java
+++ src/main/java/TestMethods/HomePageMethods.java
@@ -5,7 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
