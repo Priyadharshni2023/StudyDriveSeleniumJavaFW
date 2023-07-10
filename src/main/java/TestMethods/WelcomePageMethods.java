1dd28b92bf2520c5b213a9ae5156d7568ff2bc94
priyadharshini <rpd15996@gmail.com>
2023-07-10 21:29:34 +0200

    final commit

--- src/main/java/TestMethods/WelcomePageMethods.java
+++ src/main/java/TestMethods/WelcomePageMethods.java
@@ -4,8 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import PageObjects.WelcomePageObjects;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.List;
 
